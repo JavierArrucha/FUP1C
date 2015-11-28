8bd69001593fd616342f5921a7db1de2f4e54867
JavierArrucha <francisco95javier@hotmail.com>
2015-11-28 17:45:51 -0600

    FUP1C

---
+++ Programa_evidencia_cangrejo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int a,b,c,d,e,f=0;
+int main ()
+{
+	printf("Por favor ingresa la profundidad de pozo: \n");
+	scanf("%d",&a);
+    printf("Por favor ingresa los metros que avanza en el dia: \n");
+	scanf("%d",&b);
+	printf("Por favor ingresa los metros que retrocede de noche: \n");
+	scanf("%d",&c);
+	{
+		while(a<=d);
+		{
+			d=a-b;
+		}
+		while(a==e)
+		e=d+c;
+	}
+	f=e+1;
+	printf("los dias que tarda es: \n");
+}
